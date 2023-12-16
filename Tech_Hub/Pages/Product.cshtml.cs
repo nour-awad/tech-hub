429b9cbb64cb8d7339ac03f75f9418f6257acefb
nourwan <nour.awad094@gmail.com>
2023-12-16 21:11:41 +0200

    added SignIn.cshtml and SignIn.cshtml.cs

---
+++ Tech_Hub/Pages/Product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tech_Hub.Pages
+{
+    public class ProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Tech_Hub/Pages/Shop.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tech_Hub.Pages
+{
+    public class ShopModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
