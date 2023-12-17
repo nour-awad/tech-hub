4f1e8e46743805c37f136e94fe639508d4278261
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2023-12-18 00:06:16 +0200

    Added Track Order Page

---
+++ Tech_Hub/Pages/TrackOrder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tech_Hub.Pages
+{
+    public class TrackOrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
