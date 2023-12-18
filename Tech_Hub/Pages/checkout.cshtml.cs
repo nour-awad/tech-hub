9afac0c94f0a50a031199bebf8aae5c37290162a
Ahmed-Taha-981 <122402269+Ahmed-Taha-981@users.noreply.github.com>
2023-12-18 12:14:13 +0200

    modified:   Pages/checkout.cshtml 	modified:   Pages/checkout.cshtml.cs

--- Tech_Hub/Pages/checkout.cshtml.cs
+++ Tech_Hub/Pages/checkout.cshtml.cs
@@ -8,5 +8,15 @@ namespace Tech_Hub.Pages
         public void OnGet()
         {
         }
-    }
+		public IActionResult OnPost()
+		{
+			// Capture the current date and time
+			DateTime Ordertime = DateTime.Now;
+
+			// You can add more processing logic here, such as storing the order in a database, sending emails, etc.
+
+			// Redirect to a thank you page or any other page you'd like
+			return RedirectToPage("/ThankYou");
+		}
+	}
 }
