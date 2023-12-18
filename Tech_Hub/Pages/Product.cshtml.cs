4b1bf4156af545d5dcc9df94dd090dc3256964fe
kimosoft1 <kariiimashraf.1@gmail.com>
2023-12-18 11:30:30 +0200

    modified:   Pages/Product.cshtml.cs

--- Tech_Hub/Pages/Product.cshtml.cs
+++ Tech_Hub/Pages/Product.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace Tech_Hub.Pages
 {
     public class ProductModel : PageModel
     {
+        private List<Review> reviews = new List<Review>();
+
+        // Property to expose reviews to the Razor Page
+        public IEnumerable<Review> Reviews => reviews;
+
         public void OnGet()
         {
+            
+        }
+
+        public IActionResult OnPostSubmitReview(Review review)
+        {
+            reviews.Add(review);
+
+            return RedirectToPage();
         }
     }
 }
