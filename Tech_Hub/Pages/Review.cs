bd087f7b28d4725f22aab306c54abc459ee67462
kimosoft1 <kariiimashraf.1@gmail.com>
2023-12-18 10:59:03 +0200

    new file:   Pages/Review.cs

---
+++ Tech_Hub/Pages/Review.cs
@@ -0,0 +1,7 @@
+public class Review
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Comment { get; set; }
+    public int Rating { get; set; }
+}
