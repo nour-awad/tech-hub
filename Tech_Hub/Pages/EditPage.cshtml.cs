c97a2584d0d241286ea5bc99f6a15d5a1cbc823d
Ahmed-Taha-981 <122402269+Ahmed-Taha-981@users.noreply.github.com>
2024-01-09 21:52:43 +0200

    modified:   Tech_Hub/Pages/EditPage.cshtml 	modified:   Tech_Hub/Pages/EditPage.cshtml.cs 	modified:   Tech_Hub/Pages/UserAccount.cshtml.cs

--- Tech_Hub/Pages/EditPage.cshtml.cs
+++ Tech_Hub/Pages/EditPage.cshtml.cs
@@ -5,15 +5,26 @@ namespace Tech_Hub.Pages
 {
     public class EditPageModel : PageModel
     {
+        [BindProperty]
         public string fullname { get; set; }
 
-        public void OnGet()
+        [BindProperty]
+        public string EmailAddress { get; set; }
+
+        [BindProperty]
+        public string Phone { get; set; }
+
+        public string Street { get; set; }
+        public string City { get; set; }
+
+
+		public void OnGet()
         {
         }
 
-        public IActionResult Onpost()
+        public IActionResult OnPost()
         {
-            return RedirectToPage("/UserAccount", new {F_name = fullname });
+            return RedirectToPage("/UserAccount", new { F_name = fullname, Email = EmailAddress, P_number = Phone });
         }
     }
 }
--- Tech_Hub/Pages/UserAccount.cshtml.cs
+++ Tech_Hub/Pages/UserAccount.cshtml.cs
@@ -20,6 +20,10 @@ public class UserAccount : PageModel
 
     public void OnGet()
     {
+       
         
+       
+
     }
+
 }
